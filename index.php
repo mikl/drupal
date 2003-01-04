058222b426b982a40002bd1592146b45a1e379a7
dries <dries>
2003-01-04 23:47:10 +0000

    - Added a check for register_globals.

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-// $Id: index.php,v 1.54.2.1 2002/12/26 12:14:53 dries Exp $
+// $Id: index.php,v 1.54.2.2 2003/01/04 23:47:10 dries Exp $
 
 include_once "includes/common.inc";
 
 page_header();
 
 check_php_setting("magic_quotes_gpc", 0);
+check_php_setting("register_globals", 1);
 
 if (module_hook(variable_get("site_frontpage", "node"), "page")) {
   module_invoke(variable_get("site_frontpage", "node"), "page");

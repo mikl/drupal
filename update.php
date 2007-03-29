a6e28d4ab3e9ca245023b5644a3c3c05c7181b55
drumm <drumm>
2007-03-29 05:18:55 +0000

    Patch #131483 by kkaefer: E_NOTICE cleanup. Backport from HEAD.

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: update.php,v 1.211 2006/12/25 21:22:03 drumm Exp $
+// $Id: update.php,v 1.211.2.1 2007/03/29 05:18:55 drumm Exp $
 
 /**
  * @file
@@ -391,7 +391,9 @@ function update_update_page() {
   }
 
   // Keep track of total number of updates
-  $_SESSION['update_total'] = count($_SESSION['update_remaining']);
+  if (isset($_SESSION['update_remaining'])) {
+    $_SESSION['update_total'] = count($_SESSION['update_remaining']);
+  }
 
   if ($_POST['has_js']) {
     return update_progress_page();
@@ -529,7 +531,7 @@ function update_finished_page($success) {
   $output .= theme('item_list', $links);
 
   // Output a list of queries executed
-  if ($_SESSION['update_results']) {
+  if (!empty($_SESSION['update_results'])) {
     $output .= '<div id="update-results">';
     $output .= '<h2>The following queries were executed</h2>';
     foreach ($_SESSION['update_results'] as $module => $updates) {
